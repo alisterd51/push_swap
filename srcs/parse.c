59c957e31d0892c87b28cd1437984ac6face3d32
anclarma <anclarma@student.42.fr>
2021-05-21 21:36:38 +0200

    test

--- srcs/parse.c
+++ srcs/parse.c
@@ -6,7 +6,7 @@
 /*   By: anclarma <anclarma@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/13 14:39:13 by anclarma          #+#    #+#             */
-/*   Updated: 2021/05/21 19:11:01 by anclarma         ###   ########.fr       */
+/*   Updated: 2021/05/21 21:29:34 by anclarma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,7 +30,7 @@ static int	parse_arg(t_pile **a, char *str)
 			str++;
 		while (ft_isspace(*str))
 			str++;
-		if (*str)
+		if (*str && *str != '-' && !ft_isdigit(*str))
 			return (1);
 	}
 	return (0);
