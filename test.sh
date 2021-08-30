2b508192bb36b03abe32534b93dd33c1737c859e
anclarma <anclarma@student.42.fr>
2021-08-30 15:38:14 +0200

    correction suite a analyse par codefactor

--- test.sh
+++ test.sh
@@ -1,31 +1,31 @@
-#/bin/sh
+#!/bin/zsh
 echo "test 3"
-export ARG=`ruby -e "puts (1..3).to_a.shuffle.join(' ')"` ; ./push_swap $ARG | wc -l
-export ARG=`ruby -e "puts (1..3).to_a.shuffle.join(' ')"` ; ./push_swap $ARG | wc -l
-export ARG=`ruby -e "puts (1..3).to_a.shuffle.join(' ')"` ; ./push_swap $ARG | wc -l
-export ARG=`ruby -e "puts (1..3).to_a.shuffle.join(' ')"` ; ./push_swap $ARG | wc -l
-export ARG=`ruby -e "puts (1..3).to_a.shuffle.join(' ')"` ; ./push_swap $ARG | wc -l
+export ARG=$(ruby -e "puts (1..3).to_a.shuffle.join(' ')") ; ./push_swap $ARG | wc -l
+export ARG=$(ruby -e "puts (1..3).to_a.shuffle.join(' ')") ; ./push_swap $ARG | wc -l
+export ARG=$(ruby -e "puts (1..3).to_a.shuffle.join(' ')") ; ./push_swap $ARG | wc -l
+export ARG=$(ruby -e "puts (1..3).to_a.shuffle.join(' ')") ; ./push_swap $ARG | wc -l
+export ARG=$(ruby -e "puts (1..3).to_a.shuffle.join(' ')") ; ./push_swap $ARG | wc -l
 echo "test 5"
-export ARG=`ruby -e "puts (1..5).to_a.shuffle.join(' ')"` ; ./push_swap $ARG | wc -l
-export ARG=`ruby -e "puts (1..5).to_a.shuffle.join(' ')"` ; ./push_swap $ARG | wc -l
-export ARG=`ruby -e "puts (1..5).to_a.shuffle.join(' ')"` ; ./push_swap $ARG | wc -l
-export ARG=`ruby -e "puts (1..5).to_a.shuffle.join(' ')"` ; ./push_swap $ARG | wc -l
-export ARG=`ruby -e "puts (1..5).to_a.shuffle.join(' ')"` ; ./push_swap $ARG | wc -l
+export ARG=$(ruby -e "puts (1..5).to_a.shuffle.join(' ')") ; ./push_swap $ARG | wc -l
+export ARG=$(ruby -e "puts (1..5).to_a.shuffle.join(' ')") ; ./push_swap $ARG | wc -l
+export ARG=$(ruby -e "puts (1..5).to_a.shuffle.join(' ')") ; ./push_swap $ARG | wc -l
+export ARG=$(ruby -e "puts (1..5).to_a.shuffle.join(' ')") ; ./push_swap $ARG | wc -l
+export ARG=$(ruby -e "puts (1..5).to_a.shuffle.join(' ')") ; ./push_swap $ARG | wc -l
 echo "test 10"
-export ARG=`ruby -e "puts (1..10).to_a.shuffle.join(' ')"` ; ./push_swap $ARG | wc -l
-export ARG=`ruby -e "puts (1..10).to_a.shuffle.join(' ')"` ; ./push_swap $ARG | wc -l
-export ARG=`ruby -e "puts (1..10).to_a.shuffle.join(' ')"` ; ./push_swap $ARG | wc -l
-export ARG=`ruby -e "puts (1..10).to_a.shuffle.join(' ')"` ; ./push_swap $ARG | wc -l
-export ARG=`ruby -e "puts (1..10).to_a.shuffle.join(' ')"` ; ./push_swap $ARG | wc -l
+export ARG=$(ruby -e "puts (1..10).to_a.shuffle.join(' ')") ; ./push_swap $ARG | wc -l
+export ARG=$(ruby -e "puts (1..10).to_a.shuffle.join(' ')") ; ./push_swap $ARG | wc -l
+export ARG=$(ruby -e "puts (1..10).to_a.shuffle.join(' ')") ; ./push_swap $ARG | wc -l
+export ARG=$(ruby -e "puts (1..10).to_a.shuffle.join(' ')") ; ./push_swap $ARG | wc -l
+export ARG=$(ruby -e "puts (1..10).to_a.shuffle.join(' ')") ; ./push_swap $ARG | wc -l
 echo "test 100"
-export ARG=`ruby -e "puts (1..100).to_a.shuffle.join(' ')"` ; ./push_swap $ARG | wc -l
-export ARG=`ruby -e "puts (1..100).to_a.shuffle.join(' ')"` ; ./push_swap $ARG | wc -l
-export ARG=`ruby -e "puts (1..100).to_a.shuffle.join(' ')"` ; ./push_swap $ARG | wc -l
-export ARG=`ruby -e "puts (1..100).to_a.shuffle.join(' ')"` ; ./push_swap $ARG | wc -l
-export ARG=`ruby -e "puts (1..100).to_a.shuffle.join(' ')"` ; ./push_swap $ARG | wc -l
+export ARG=$(ruby -e "puts (1..100).to_a.shuffle.join(' ')") ; ./push_swap $ARG | wc -l
+export ARG=$(ruby -e "puts (1..100).to_a.shuffle.join(' ')") ; ./push_swap $ARG | wc -l
+export ARG=$(ruby -e "puts (1..100).to_a.shuffle.join(' ')") ; ./push_swap $ARG | wc -l
+export ARG=$(ruby -e "puts (1..100).to_a.shuffle.join(' ')") ; ./push_swap $ARG | wc -l
+export ARG=$(ruby -e "puts (1..100).to_a.shuffle.join(' ')") ; ./push_swap $ARG | wc -l
 echo "test 500"
-export ARG=`ruby -e "puts (1..500).to_a.shuffle.join(' ')"` ; ./push_swap $ARG | wc -l
-export ARG=`ruby -e "puts (1..500).to_a.shuffle.join(' ')"` ; ./push_swap $ARG | wc -l
-export ARG=`ruby -e "puts (1..500).to_a.shuffle.join(' ')"` ; ./push_swap $ARG | wc -l
-export ARG=`ruby -e "puts (1..500).to_a.shuffle.join(' ')"` ; ./push_swap $ARG | wc -l
-export ARG=`ruby -e "puts (1..500).to_a.shuffle.join(' ')"` ; ./push_swap $ARG | wc -l
+export ARG=$(ruby -e "puts (1..500).to_a.shuffle.join(' ')") ; ./push_swap $ARG | wc -l
+export ARG=$(ruby -e "puts (1..500).to_a.shuffle.join(' ')") ; ./push_swap $ARG | wc -l
+export ARG=$(ruby -e "puts (1..500).to_a.shuffle.join(' ')") ; ./push_swap $ARG | wc -l
+export ARG=$(ruby -e "puts (1..500).to_a.shuffle.join(' ')") ; ./push_swap $ARG | wc -l
+export ARG=$(ruby -e "puts (1..500).to_a.shuffle.join(' ')") ; ./push_swap $ARG | wc -l
