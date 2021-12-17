56dad6d3e9c422a8317f3ac9118436584ef622b5
Antoine CLARMAN <anclarma@e2r7p18.clusters.42paris.fr>
2021-12-17 14:51:03 +0100

    correction https://github.com/koalaman/shellcheck/wiki/SC2181

--- error_cases.sh
+++ error_cases.sh
@@ -26,20 +26,36 @@ function checkNoArgs
 	printf $WHITE
 
 	printf "%-*s: " $PAD "$EXEC"
-	$EXEC > /dev/null 2>&1
-	if [[ $? -eq 0 ]] ; then OK ; else KO ; fi
+	if $EXEC > /dev/null 2>&1
+	then
+		OK
+	else
+		KO
+	fi
 
 	printf "%-*s: " $PAD "$EXEC \"\""
-	$EXEC "" > /dev/null 2>&1
-	if [[ $? -eq 0 ]] ; then OK ; else KO ; fi
+	if $EXEC "" > /dev/null 2>&1
+	then
+		OK
+	else
+		KO
+	fi
 
 	printf "%-*s: " $PAD "$EXEC \"           \""
-	$EXEC "           " > /dev/null 2>&1
-	if [[ $? -eq 0 ]] ; then OK ; else KO ; fi
+	if $EXEC "           " > /dev/null 2>&1
+	then
+		OK
+	else
+		KO
+	fi
 
 	printf "%-*s: " $PAD "$EXEC \"   \" \"     \""
-	$EXEC "   " "     " > /dev/null 2>&1
-	if [[ $? -eq 0 ]] ; then OK ; else KO ; fi
+	if $EXEC "   " "     " > /dev/null 2>&1
+	then
+		OK
+	else
+		KO
+	fi
 }
 
 # Not digital
@@ -52,24 +68,44 @@ function checkNotDigital
 	printf $WHITE
 
 	printf "%-*s: " $PAD "$EXEC abc"
-	$EXEC abc 2>&1 | grep --quiet "Error"
-	if [[ $? -eq 0 ]] ; then OK ; else KO ; fi
+	if $EXEC abc 2>&1 | grep --quiet "Error"
+	then
+		OK
+	else
+		KO
+	fi
 
 	printf "%-*s: " $PAD "$EXEC 1 2 3%*"
-	$EXEC 1 2 3%* 2>&1 | grep --quiet "Error"
-	if [[ $? -eq 0 ]] ; then OK ; else KO ; fi
+	if $EXEC 1 2 3%* 2>&1 | grep --quiet "Error"
+	then
+		OK
+	else
+		KO
+	fi
 
 	printf "%-*s: " $PAD "$EXEC 1 2 3 lol"
-	$EXEC 1 2 3 lol 2>&1 | grep --quiet "Error"
-	if [[ $? -eq 0 ]] ; then OK ; else KO ; fi
+	if $EXEC 1 2 3 lol 2>&1 | grep --quiet "Error"
+	then
+		OK
+	else
+		KO
+	fi
 
 	printf "%-*s: " $PAD "$EXEC \"-\" \"5 42\""
-	$EXEC "-" "5 42" 2>&1 | grep --quiet "Error"
-	if [[ $? -eq 0 ]] ; then OK ; else KO ; fi
+	if $EXEC "-" "5 42" 2>&1 | grep --quiet "Error"
+	then
+		OK
+	else
+		KO
+	fi
 
 	printf "%-*s: " $PAD "$EXEC \"1 2\" \"3\" \"@#$%^\""
-	$EXEC "1 2" "3" "@#$%^" 2>&1 | grep --quiet "Error"
-	if [[ $? -eq 0 ]] ; then OK ; else KO ; fi
+	if $EXEC "1 2" "3" "@#$%^" 2>&1 | grep --quiet "Error"
+	then
+		OK
+	else
+		KO
+	fi
 }
 
 # Values higher than INT_MAX or lower than INT_MIN
@@ -82,24 +118,44 @@ function checkOverLimitsInt
 	printf $WHITE
 
 	printf "%-*s: " $PAD "$EXEC 2147483648"
-	$EXEC 2147483648 2>&1 | grep --quiet "Error"
-	if [[ $? -eq 0 ]] ; then OK ; else KO ; fi
+	if $EXEC 2147483648 2>&1 | grep --quiet "Error"
+	then
+		OK
+	else
+		KO
+	fi
 
 	printf "%-*s: " $PAD "$EXEC \"   -2147483649\""
-	$EXEC "   -2147483649" 2>&1 | grep --quiet "Error"
-	if [[ $? -eq 0 ]] ; then OK ; else KO ; fi
+	if $EXEC "   -2147483649" 2>&1 | grep --quiet "Error"
+	then
+		OK
+	else
+		KO
+	fi
 
 	printf "%-*s: " $PAD "$EXEC \"4294967295   \""
-	$EXEC "4294967295   " 2>&1 | grep --quiet "Error"
-	if [[ $? -eq 0 ]] ; then OK ; else KO ; fi
+	if $EXEC "4294967295   " 2>&1 | grep --quiet "Error"
+	then
+		OK
+	else
+		KO
+	fi
 
 	printf "%-*s: " $PAD "$EXEC \"  -9223372036854775808  \""
-	$EXEC "  -9223372036854775808  " 2>&1 | grep --quiet "Error"
-	if [[ $? -eq 0 ]] ; then OK ; else KO ; fi
+	if $EXEC "  -9223372036854775808  " 2>&1 | grep --quiet "Error"
+	then
+		OK
+	else
+		KO
+	fi
 
 	printf "%-*s: " $PAD "$EXEC -18446744073709551615"
-	$EXEC "-18446744073709551615" 2>&1 | grep --quiet "Error"
-	if [[ $? -eq 0 ]] ; then OK ; else KO ; fi
+	if $EXEC "-18446744073709551615" 2>&1 | grep --quiet "Error"
+	then
+		OK
+	else
+		KO
+	fi
 }
 
 function checkNotUniq
@@ -111,16 +167,28 @@ function checkNotUniq
 	printf $WHITE
 
 	printf "%-*s: " $PAD "$EXEC 1 2 3 4 5 6 7 8 9 1"
-	$EXEC 1 2 3 4 5 6 7 8 9 1 2>&1 | grep --quiet "Error" 2>&1
-	if [[ $? -eq 0 ]] ; then OK ; else KO ; fi
+	if $EXEC 1 2 3 4 5 6 7 8 9 1 2>&1 | grep --quiet "Error" 2>&1
+	then
+		OK
+	else
+		KO
+	fi
 
 	printf "%-*s: " $PAD "$EXEC \"-001 02\" \"3 4 5 6 7 8 9\" -1"
-	$EXEC "-001 2" "3 4 5 6 7 8 9" -1 2>&1 | grep --quiet "Error" 2>&1
-	if [[ $? -eq 0 ]] ; then OK ; else KO ; fi
+	if $EXEC "-001 2" "3 4 5 6 7 8 9" -1 2>&1 | grep --quiet "Error" 2>&1
+	then
+		OK
+	else
+		KO
+	fi
 
 	printf "%-*s: " $PAD "$EXEC \"-000\" \"+0\""
-	$EXEC "-000" "+0" 2>&1 | grep --quiet "Error" 2>&1
-	if [[ $? -eq 0 ]] ; then OK ; else KO ; fi
+	if $EXEC "-000" "+0" 2>&1 | grep --quiet "Error" 2>&1
+	then
+		OK
+	else
+		KO
+	fi
 }
 
 function checkStderr
@@ -132,12 +200,20 @@ function checkStderr
 	printf $WHITE
 
 	printf "%-*s: " $PAD "$EXEC 1 2 3 4 5 6 7 8 9 1"
-	$EXEC 1 2 3 4 5 6 7 8 9 1 > /dev/null 2>$TMP ; grep --quiet "Error" $TMP
-	if [[ $? -eq 0 ]] ; then OK ; else KO ; fi
+	if $EXEC 1 2 3 4 5 6 7 8 9 1 > /dev/null 2>$TMP ; grep --quiet "Error" $TMP
+	then
+		OK
+	else
+		KO
+	fi
 
 	printf "%-*s: " $PAD "$EXEC abc"
-	$EXEC abc > /dev/null 2>$TMP ; grep --quiet "Error" $TMP
-	if [[ $? -eq 0 ]] ; then OK ; else KO ; fi
+	if $EXEC abc > /dev/null 2>$TMP ; grep --quiet "Error" $TMP
+	then
+		OK
+	else
+		KO
+	fi
 
 	rm $TMP
 }
